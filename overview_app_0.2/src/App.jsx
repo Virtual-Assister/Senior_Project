76a7fae582cdb53a4d5c97e5427212a83181e7c3
Jordan Snow <jsnow7@msudenver.edu>
2024-02-15 14:36:47 -0700

    second first push

---
+++ overview_app_0.2/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './header.jsx'
+import Footer from './footer.jsx'
+
+function App() {
+
+  return(
+    <>
+    <Header/>
+    <Footer/>
+    </>
+  )
+  
+}
+
+export default App
---
+++ overview_app_0.2/src/footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Degree Overview Generator</p>
+        </footer>
+    )
+}
+
+export default Footer
