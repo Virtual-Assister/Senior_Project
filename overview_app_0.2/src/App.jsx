34939de9cac8a835a4df12487cd8436563920a59
Jordan Snow <jsnow7@msudenver.edu>
2024-02-27 12:09:01 -0700

    minors changes to an error on a file

--- overview_app_0.2/src/App.jsx
+++ overview_app_0.2/src/App.jsx
@@ -2,7 +2,7 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Body from "./components/Body.jsx";
 import BodyComponent from "./components/Body_2.jsx";
-import gridButton from "./components/hyperlink_button.jsx";
+
 
 function App() {
   return (
