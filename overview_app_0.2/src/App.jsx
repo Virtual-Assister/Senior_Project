1e9133115e931dc5ff59c2151904c7105438c929
Jordan Snow <jordantreysnow@gmail.com>
2024-02-27 09:18:45 -0700

    button 2 and style changes

--- overview_app_0.2/src/App.jsx
+++ overview_app_0.2/src/App.jsx
@@ -1,12 +1,16 @@
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Body from "./components/Body.jsx";
+import BodyComponent from "./components/Body_2.jsx";
+import gridButton from "./components/hyperlink_button.jsx";
 
 function App() {
   return (
     <>
+      <gridButton/>
       <Header />
-      <Body />
+      <Body/>
+      <BodyComponent/>
       <Footer />
     </>
   );
