cf748459cc194cce80fe391639e223be46725339
Jordan Snow <jsnow7@msudenver.edu>
2024-03-12 15:16:45 -0600

    more dependency changes

--- overview_app_0.2/vite.config.js
+++ overview_app_0.2/vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Degree_Overview.github.io",
-  plugins: [react()],
+  build: {
+    base: "/Degree_Overview.github.io",
+    plugins: [react()],
+  }
 })
